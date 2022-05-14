bd4c0977f48e9be967a20c0d3e0b9a3d864d2761
Artem Nikulin <itsacats@gmail.com>
2022-05-14 23:23:40 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+    Route, NavLink
+} from "react-router-dom";
+import {Routes} from "react-router";
+import Home from './Home';
+import About from './About';
+import Dashboard from './Dashboard';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  render() {
+    return (
+        <div>
+            <ul>
+                <li>
+                    <NavLink  to="/" >Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">About</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/dashboard">Dashboard</NavLink>
+                </li>
+            </ul>
+
+            <hr />
+            <Routes>
+                <Route  path="/" element={<Home  />} />
+
+                <Route path="/about" element={<About />}/>
+
+                <Route path="/dashboard" element={<Dashboard />}/>
+
+            </Routes>
+        </div>
+    );
+  }
+
+
 }
 
 export default App;
